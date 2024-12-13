03b338259b8dd83af3afb47542ded8948747dd46
Muhammad Saad <saadjameel2001@gmail.com>
2024-12-13 21:08:10 +0500

    corrction in utilities

--- ecom_base/app/utils.py
+++ ecom_base/app/utils.py
@@ -13,13 +13,13 @@ def get_password_hash(password: str)-> str:
     return pwd_context.hash(password)
 
 #function to verify password 
-def verify_password(plain_password, hashed_password)-> bool:
+def verify_password(plain_password: str, hashed_password:str)-> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 #function to create the access token
 def create_access_token(data:dict)->str:
     to_encode = data.copy()
-    expire = datetime.utcnow + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)  
+    expire = datetime.utcnow() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)  
     to_encode.update({"exp": expire}) #add expiration time to the  token 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm = ALGORITHM) #generate the token 
     return encoded_jwt
