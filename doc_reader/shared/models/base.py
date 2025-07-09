b07872a456f05862d5feffe79fd18877ff85682f
Muhammad-Saad-2 <saadjameel2001@gmail.com>
2025-07-09 15:45:42 +0500

    fix: table create issue

--- doc_reader/shared/models/base.py
+++ doc_reader/shared/models/base.py
@@ -1,7 +1,8 @@
 from sqlmodel import SQLModel, Field, Relationship
 from datetime import datetime, timezone, timedelta
 class Base(SQLModel):
-    pass 
+    pass
+    
 
 class User(Base ,table = True):
     id: int | None = Field(default = None, primary_key = True)
@@ -26,8 +27,10 @@ class User(Base ,table = True):
 class Profile(Base, table = True):
     full_name: str = Field(max_length=50, nullable=False)
     bio: str | None = Field(max_length=120)
-    profile_id: int = Field(foreign_key="user.id", unique=True)
+    user_id: int = Field(foreign_key="user.id", unique=True)
     user: User = Relationship(back_populates="profile")
+    profile_id : int = Field(primary_key=True, index=True, unique=True)
+    
 
 
 class OTP(Base, table = True ):
@@ -40,3 +43,9 @@ class OTP(Base, table = True ):
     is_used : bool = Field(default=False)
     user_id: int = Field(foreign_key="user.id", index=True)
     user: User = Relationship(back_populates="otps")
+    otp_id: int = Field(primary_key=True, index=True, unique=True)
+
+
+
+def get_base():
+    return Base
--- doc_reader/shared/utils/database.py
+++ doc_reader/shared/utils/database.py
@@ -1,13 +1,13 @@
 from shared.config.settings import get_settings
-from shared.models.base import Base
+from shared.models.base import get_base
 import redis
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from motor.motor_asyncio import AsyncIOMotorClient 
 from pymongo.server_api import ServerApi
 from pymongo import AsyncMongoClient
 import asyncio
 
 settings = get_settings()
+base = get_base()
 
 '''Esatablish connection with redis'''
 r = redis.Redis(
@@ -35,11 +35,22 @@ AsyncSessionLocal = async_sessionmaker(
     expire_on_commit=False
 )
 
-'''Establish connection with mongodb'''
-async def create_table():
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
 
+async def create_table():
+    try:
+        async with async_engine.begin() as conn:
+            await conn.run_sync(base.metadata.create_all)
+    except Exception as e:
+         print(e)
+    finally:
+         conn.close()
+
+asyncio.run(create_table())
+
+
+
+
+'''Establish connection with mongodb'''
 async def connect_mongo():
         mongodb = AsyncMongoClient(settings.MONGODB_URL, server_api = ServerApi(
             version= '1', strict=True, deprecation_errors=True
